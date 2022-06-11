aabb655296772498394afdd1c5c0f647151ffee6
neychok <n.kalaidjiev@gmail.com>
2022-06-12 00:13:36 +0300

    Added a basic Slack and Asana integration.

--- index.js
+++ index.js
@@ -1,15 +1,41 @@
 const { App } = require("@slack/bolt");
+const logger = require("pino")();
+const Asana = require("asana");
 
-const app = new App({
+if (process.env.NODE_ENV !== "production") {
+  require("dotenv").config();
+}
+
+const slack = new App({
   signingSecret: process.env.SLACK_SIGNING_SECRET,
   token: process.env.SLACK_BOT_TOKEN,
+  socketMode: true,
+  appToken: process.env.SLACK_APP_TOKEN,
+  port: process.env.PORT || 6969,
 });
 
-/* Add functionality here */
+const asana = Asana.Client.create().useAccessToken(
+  process.env.ASANA_PERSONAL_TOKEN
+);
 
+/* Add functionality here */
+slack.message("", async ({ message, say }) => {
+  const username = await asana.users.me().then((me) => {
+    return me.name;
+  });
+
+  await say(`Hey there, ${username}!`);
+});
+
+// Start bot
 (async () => {
   // Start the app
-  await app.start(process.env.PORT || 3000);
-
-  console.log("⚡️ Bolt app is running!");
+  await slack.start();
+  logger.info("⚡️ Bolt app is running!");
 })();
+
+process.on("SIGINT", function () {
+  logger.info("\nGracefully shutting down from SIGINT (Ctrl-C)");
+  // some other closing procedures go here
+  process.exit();
+});
